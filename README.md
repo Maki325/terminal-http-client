148e50eef20340096fe3d36f6052bf03386a0835
Marko <markoni521@gmail.com>
2025-02-09 18:31:19 +0100

    Made Text component, and render it to top left

---
+++ README.md
@@ -0,0 +1,7 @@
+# Terminal HTTP Client
+
+## TODO
+
+- [ ] Make a `Container` component, that can be padded, can align items, etc, etc...
+- [ ] Make a `Border` component
+- [ ] Rework the `Text` component to not have a border. `Border` component should be used to add a border
--- src/main.zig
+++ src/main.zig
@@ -10,75 +10,64 @@ pub fn main() !void {
         _ = gpa.deinit();
     }
 
-    const screen = try Screen.init(alloc);
-    // std.debug.print("Screen: {any}\n", .{screen});
+    var screen = try Screen.init(alloc);
+    defer screen.deinit();
 
-    // try normalScreen(screen);
+    const text = try alloc.dupe(u8, "0123456789");
+    defer alloc.free(text);
+    screen.render(try Text.init(text, 10));
+
     try privateANSIMode(screen);
 }
 
 fn privateANSIMode(screen: Screen) !void {
-    // Save screen
-    // std.debug.print("\x1b[?47h", .{});
-
     // Enables the alternative buffer
     std.debug.print("\x1b[?1049h", .{});
     // Go to 0:0
     std.debug.print("\x1b[0;0H", .{});
 
-    // Go to 0:0 & Clear screen (not necessary when using alternative buffer)
-    // std.debug.print("\x1b[0;0H", .{});
-    // std.debug.print("\x1b[J", .{});
-    // std.debug.print("\x1b[0;0H", .{});
-
     for (0..screen.height) |y| {
         if (y != 0) {
             std.debug.print("\n", .{});
         }
         for (0..screen.width) |x| {
-            _ = x;
-            std.debug.print("a", .{});
-            std.time.sleep(250 * std.time.ns_per_us);
+            const char = &screen.buffer[y * screen.width + x];
+            if (char.data[0] == 0) {
+                std.debug.print(" ", .{});
+            } else {
+                for (char.data) |c| {
+                    if (c == 0) break;
+                    std.debug.print("{c}", .{c});
+                }
+            }
         }
     }
 
-    // std.debug.print("\x1b[0;0H", .{});
-    // Clear screen
-    // std.debug.print("\x1b[J", .{});
+    std.time.sleep(1 * std.time.ns_per_s);
 
     // Disable the alternative buffer
     std.debug.print("\x1b[?1049l", .{});
-
-    // Restore screen
-    // std.debug.print("\x1b[?47l", .{});
 }
 
-fn normalScreen(screen: Screen) !void {
-    for (0..screen.height - 1) |_| {
-        std.debug.print("\n", .{});
+const Char = struct {
+    // This is so there's enough bytes to do ANSI Escape Codes before the actual char
+    // We'll mostly be skipping 11/12 chars, if not 12/12
+    // Since it's all in linear memory skipping should not slow it down (that much)
+    data: [12]u8,
+
+    fn fromChar(c: u8) Char {
+        var char = std.mem.zeroes(Char);
+        char.data[0] = c;
+        return char;
     }
-
-    std.debug.print("\x1b[{d}A", .{screen.height});
-    std.debug.print("\x1b[0;0H", .{});
-
-    for (0..screen.height) |y| {
-        if (y != 0) {
-            std.debug.print("\n", .{});
-        }
-        for (0..screen.width) |x| {
-            _ = x;
-            std.debug.print("a", .{});
-            std.time.sleep(250 * std.time.ns_per_us);
-        }
-    }
-
-    std.debug.print("\x1b[0;0H", .{});
-    std.debug.print("\x1b[J", .{});
-}
+};
 
 const Screen = struct {
     width: usize,
     height: usize,
+
+    alloc: std.mem.Allocator,
+    buffer: []Char,
 
     fn init(alloc: std.mem.Allocator) !Screen {
         const resultWidth = try std.process.Child.run(.{ .allocator = alloc, .argv = &.{ "tput", "cols" } });
@@ -86,18 +75,96 @@ const Screen = struct {
             alloc.free(resultWidth.stderr);
             alloc.free(resultWidth.stdout);
         }
-        const width = resultWidth.stdout;
+        const widthStr = resultWidth.stdout;
+        const width = std.fmt.parseInt(usize, widthStr[0 .. widthStr.len - 1], 10) catch return error.UnableToParseWidth;
 
         const resultHeight = try std.process.Child.run(.{ .allocator = alloc, .argv = &.{ "tput", "lines" } });
         defer {
             alloc.free(resultHeight.stderr);
             alloc.free(resultHeight.stdout);
         }
-        const height = resultHeight.stdout;
+        const heightStr = resultHeight.stdout;
+        const height = std.fmt.parseInt(usize, heightStr[0 .. heightStr.len - 1], 10) catch return error.UnableToParseHeight;
+
+        const buffer = try alloc.alloc(Char, width * height);
+        @memset(buffer, std.mem.zeroes(Char));
 
         return .{
-            .width = std.fmt.parseInt(usize, width[0 .. width.len - 1], 10) catch return error.UnableToParseWidth,
-            .height = std.fmt.parseInt(usize, height[0 .. height.len - 1], 10) catch return error.UnableToParseHeight,
+            .width = width,
+            .height = height,
+            .alloc = alloc,
+            .buffer = buffer,
         };
+    }
+
+    fn deinit(self: *Screen) void {
+        self.alloc.free(self.buffer);
+    }
+
+    fn render(self: *Screen, component: anytype) void {
+        component.render(self, 0, 0);
+    }
+};
+
+const Text = struct {
+    value: []u8,
+    max_chars: usize,
+
+    fn init(text: []u8, max_chars: usize) !Text {
+        return .{
+            .value = text,
+            .max_chars = max_chars,
+        };
+    }
+
+    fn render(self: *const Text, screen: *Screen, x: usize, y: usize) void {
+        for (0..self.max_chars + 2) |i| {
+            screen.buffer[y * screen.width + x + i] = Char.fromChar('-');
+        }
+
+        screen.buffer[(y + 1) * screen.width + x] = Char.fromChar('|');
+        for (self.value, 0..) |c, i| {
+            if (i >= self.max_chars) {
+                break;
+            }
+
+            if (i == 0) {
+                var char = std.mem.zeroes(Char);
+                char.data[0] = '\x1b';
+                char.data[1] = '[';
+                char.data[2] = '1';
+                char.data[3] = ';';
+                char.data[4] = '3';
+                char.data[5] = '1';
+                char.data[6] = 'm';
+                char.data[7] = c;
+
+                if (self.value.len == 1) {
+                    char.data[8] = '\x1b';
+                    char.data[9] = '[';
+                    char.data[10] = '0';
+                    char.data[11] = 'm';
+                }
+
+                screen.buffer[(y + 1) * screen.width + x + 1 + i] = char;
+            } else if (i == self.value.len - 1 or i == self.max_chars - 1) {
+                var char = std.mem.zeroes(Char);
+                char.data[0] = c;
+                char.data[1] = '\x1b';
+                char.data[2] = '[';
+                char.data[3] = '0';
+                char.data[4] = 'm';
+
+                screen.buffer[(y + 1) * screen.width + x + 1 + i] = char;
+            } else {
+                screen.buffer[(y + 1) * screen.width + x + 1 + i] = Char.fromChar(c);
+            }
+        }
+
+        screen.buffer[(y + 1) * screen.width + x + 1 + self.max_chars] = Char.fromChar('|');
+
+        for (0..self.max_chars + 2) |i| {
+            screen.buffer[(y + 2) * screen.width + x + i] = Char.fromChar('-');
+        }
     }
 };
